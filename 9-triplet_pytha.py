ce04a52c0ca611833277751cb7765ae6d58a2d68
xRemixApplix <r3m1.appli@gmail.com>
2019-09-05 23:20:15 +0200

    Probleme 9 : Plus produit de nombre pythagoriens dont a+b+c=3600

---
+++ 9-triplet_pytha.py
@@ -0,0 +1,15 @@
+#-*- coding: utf8 -*-
+
+import math
+prod=0
+
+for i in range(1, int(1800)):
+    for j in range (i, 1800):
+        k = int(math.sqrt(i**2+j**2))
+        if i+j+k==3600 and i**2+j**2==k**2 and i*j*k>prod:
+            prod=i*j*k
+            #### CODE A RAJOUTER POUR DEBUGGER ####
+            #print("{}, {} et {}".format(i, j, k))
+            #######################################
+
+print("{}".format(prod))
